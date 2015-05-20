6ceb68c2b818a6fc99c5e758884bf87bdff90995
David Laws <lawsda01@gmail.com>
2015-05-20 01:09:24 -0400

    Updated Menu Buttons and Images
    Adjusted images to better display Hover/Pressed/Neutral on all buttons.
    Adjusted image sizes for unity. Added buttonUI and a control script to
    the Icon and Coin.

---
+++ Assets/Scripts/Controllers/URLButtonControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class URLButtonControl : MonoBehaviour {
+	
+	public bool isHomepageButton;
+	public bool isCoinButton;
+
+	public void visitURL(){
+		if (isHomepageButton) {
+			Application.OpenURL("http://groovegame.com");
+		}
+
+		if (isCoinButton) {
+			Application.OpenURL("http://groovegame.com");
+		}
+	}
+}
