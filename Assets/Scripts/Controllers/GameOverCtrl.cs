d65e7dd06b7ed78dc8978445f78bd19d37981e65
David Laws <lawsda01@gmail.com>
2015-05-20 22:34:26 -0400

    Game over prefab
    Built game over prefab, currently on display in demo scene.

---
+++ Assets/Scripts/Controllers/GameOverCtrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverCtrl : MonoBehaviour {
+
+	private string winText, loseText;
+
+	public void Start(){
+		winText = "You Win! \nGreat job getting your groove on!";
+		loseText = "Nice Try! \n Better luck next time!";
+	}
+
+	public void formatGameOver(bool isWin, int score, int coins){
+		GameObject.Find ("GameOverText").GetComponent<Text> ().text = (isWin) ? winText : loseText;
+
+		GameObject.Find ("Score Text").GetComponent<Text> ().text = score.ToString ();
+
+		GameObject.Find ("Coins Earned").GetComponent<Text> ().text = coins.ToString ();
+
+		GameObject.Find ("GameOverCanvas").GetComponent<Canvas> ().enabled = true;
+	}
+}
