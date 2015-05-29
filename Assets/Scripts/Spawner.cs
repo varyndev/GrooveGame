f742c344f9ba6e25c0d14bc849234c6d1e78b6db
kbenderoth <kbenderoth@ymail.com>
2015-05-28 20:38:54 -0400

    Locked position of Spawner
    Spawner's Location in each scene is now a tweakable distance relative to
    the camera's forward vector

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 namespace BoogieDownGames{
 
+	/// <summary>
+	/// Spawner.
+	/// Responsible for creating and distributing notes
+	/// </summary>
+	[SerializePrivateVariables]
 	public class Spawner : MonoBehaviour {
+
+		/// <summary>
+		/// The distance between the Spawner and the Camera about the Z axis
+		/// </summary>
+		private float _spawnDistance = 5f;
 
 		void Start()
 		{
+			// Spawner's prefab should be invisible
+
 			NotificationCenter.DefaultCenter.AddObserver(this, "spawnPrefab");
+
+			// Set the position based on the Main Camera's Set Position
+			Transform mainCameraTransform = Camera.main.transform;
+			transform.position = mainCameraTransform.position + (mainCameraTransform.forward*_spawnDistance);
 		}
 
-		[SerializeField]
+		/// <summary>
+		/// Prefab for the notes
+		/// </summary>
 		private GameObject m_prefab;
 
+		/// <summary>
+		/// Spawns the notes from the Spawner's position
+		/// </summary>
 		public void spawnPrefab()
 		{
 			Debug.Log ("Spawning note");
