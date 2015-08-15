8cab845b8b5864dcfea0a30bd31dede9e88afa6a
John Foster <john@varyn.com>
2015-08-15 15:49:03 -0400

    Wire unlock buttons; finish OBB downloader

--- Assets/Scripts/Controllers/StaticButtonControl.cs
+++ Assets/Scripts/Controllers/StaticButtonControl.cs
@@ -3,24 +3,45 @@ using System.Collections;
 
 public class StaticButtonControl : MonoBehaviour {
 	
-	public bool isHomepageButton, isCoinButton, isMainMenuButton, isPlayAgainButton;
+	public bool isHomepageButton, isCoinButton, isMainMenuButton, isPlayAgainButton, isModelLockButton, isSceneLockButton, isSongLockButton;
 
 	public void activateButton(){
 		if (isHomepageButton) {
 			Application.OpenURL("http://groovegame.com");
+			return;
 		}
 
 		if (isCoinButton) {
 			Application.OpenURL("http://groovegame.com/ggcoin");
+			return;
 		}
 
 		if (isMainMenuButton) {
 			Application.LoadLevel("Menu");
+			return;
 		}
 
 		if (isPlayAgainButton) {
 			Application.LoadLevel(Application.loadedLevel);
+			return;
 		}
-
+		
+		if (isModelLockButton) {
+			// TODO: link to code to begin the purchase model flow
+			Application.OpenURL("http://groovegame.com/songs");
+			return;
+		}
+		
+		if (isSceneLockButton) {
+			// TODO: link to code to begin the purchase scene flow
+			Application.OpenURL("http://groovegame.com/songs");
+			return;
+		}
+		
+		if (isSongLockButton) {
+			// TODO: link to code to begin the purchase songs flow
+			Application.OpenURL("http://groovegame.com/songs");
+			return;
+		}
 	}
 }
