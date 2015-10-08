17b1761e6f67c2e9f5e76cf0daa16fd8a3e9831a
Minh Tran <mcongt@gmail.com>
2015-10-07 21:16:41 -0700

    fix arscene

---
+++ Assets/Scripts/ArSpawner.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoogieDownGames{
+
+	/// <summary>
+	/// Spawner.
+	/// Responsible for creating and distributing notes
+	/// </summary>
+	public class ArSpawner : MonoBehaviour {
+
+		/// <summary>
+		/// The distance between the Spawner and the Camera about the Z axis
+		/// </summary>
+		public float spawnDistance = 5f;
+
+		private List<GameObject> m_notes;
+
+		void Start()
+		{
+			m_notes = new List<GameObject> ();
+
+			// Spawner's prefab should be invisible
+
+			NotificationCenter.DefaultCenter.AddObserver(this, "spawnPrefab");
+			NotificationCenter.DefaultCenter.AddObserver(this, "destroyAllPrefab");
+
+			// Set the position based on the Main Camera's Set Position
+			Transform mainCameraTransform = Camera.main.transform;
+			transform.position = mainCameraTransform.position + (mainCameraTransform.forward*spawnDistance) +
+                Vector3.down*500 + Vector3.left*200;
+		}
+
+		/// <summary>
+		/// Prefab for the notes
+		/// </summary>
+		[SerializeField]
+		private GameObject m_prefab;
+
+		/// <summary>
+		/// Spawns the notes from the Spawner's position
+		/// </summary>
+		public void spawnPrefab()
+		{
+			GameObject obj = MemoryPool.Instance.findAndGetObjs(m_prefab.name, false);
+			obj.transform.position = transform.position;
+			if(!m_notes.Contains(obj))
+				m_notes.Add (obj);
+		}
+
+		/// <summary>
+		/// Destroy all active notes
+		/// </summary>
+		public void destroyAllPrefab()
+		{
+			foreach (var obj in m_notes) 
+				obj.SetActive(false);
+		}
+	}
+}
