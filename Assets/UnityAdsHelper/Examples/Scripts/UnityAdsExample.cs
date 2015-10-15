3a583d5d19e5b585f9c02dba616a285389d921e1
David Laws <lawsda01@gmail.com>
2015-10-14 18:57:04 -0700

    UnityAdsUpdate

---
+++ Assets/UnityAdsHelper/Examples/Scripts/UnityAdsExample.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UnityAdsExample : MonoBehaviour 
+{
+	void Start ()
+	{
+		UnityAdsHelper.Initialize();
+	}
+}
---
+++ Assets/UnityAdsHelper/Scripts/UnityAdsSettings.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityAdsSettings : ScriptableObject 
+{
+	public const string defaultIosGameId = "24300";
+	public const string defaultAndroidGameId = "24299";
+
+	public string iosGameId     = null;
+	public string androidGameId = null;
+	
+	public bool enableTestMode  = true;
+	public bool showInfoLogs    = false;
+	public bool showDebugLogs   = false;
+	public bool showWarningLogs = true;
+	public bool showErrorLogs   = true;
+
+	public UnityAdsSettings ()
+	{
+		iosGameId = defaultIosGameId;
+		androidGameId = defaultAndroidGameId;
+	}
+}
